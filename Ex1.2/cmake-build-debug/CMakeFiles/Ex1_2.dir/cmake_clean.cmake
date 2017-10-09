ee57026f996a40847157f8ecaa67f6026ed62fe4
bartoszgamza <bartosz.gamza12@gmail.com>
2017-10-09 15:05:57 +0200

    chuj

---
+++ Ex1.2/cmake-build-debug/CMakeFiles/Ex1_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex1_2.dir/main.cpp.o"
+  "Ex1_2.pdb"
+  "Ex1_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex1_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
