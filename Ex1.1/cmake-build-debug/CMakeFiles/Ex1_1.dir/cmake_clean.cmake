2a9af19714d95e40717c40c6aa6f2d6ea118e939
bartoszgamza <bartosz.gamza12@gmail.com>
2017-10-09 14:51:54 +0200

    mistake

---
+++ Ex1.1/cmake-build-debug/CMakeFiles/Ex1_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex1_1.dir/main.c.o"
+  "Ex1_1.pdb"
+  "Ex1_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ex1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
