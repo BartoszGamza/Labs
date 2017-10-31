61091b1777475d22de4324f60530d8dd75a84e36
bartoszgamza <bartosz.gamza12@gmail.com>
2017-10-31 18:14:01 +0100

    3.2 Mielecki begining

---
+++ Ex3.2/cmake-build-debug/CMakeFiles/Ex3_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex3_2.dir/main.cpp.o"
+  "Ex3_2.pdb"
+  "Ex3_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex3_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ex3.2/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Vehicle {
+private:
+public:
+    void print(){
+        cout <<""<<endl;
+    }
+};
+
+class Car {
+private:
+public:
+    void print(){
+        cout <<""<<endl;
+    }
+};
+
+class Boat {
+private:
+public:
+    void print(){
+        cout <<""<<endl;
+    }
+};
+
+class Amhibious {
+private:
+public:
+    void print(){
+        cout <<""<<endl;
+    }
+};
+
+
+int main() {
+
+
+
+    return 0;
+}
