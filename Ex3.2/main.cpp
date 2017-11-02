c9c739eb3d2680bf4a26113b42e2904e6029855d
bartoszgamza <bartosz.gamza12@gmail.com>
2017-11-02 09:35:34 +0100

    TrainFactoring

--- Ex3.2/main.cpp
+++ Ex3.2/main.cpp
@@ -2,42 +2,111 @@
 
 using namespace std;
 
-class Vehicle {
-private:
+class Vehicle
+{
+
 public:
-    void print(){
-        cout <<""<<endl;
-    }
+    string Color;
+    void SetColor (string Color){
+        this->Color = Color;
+
+    };
+
+    void PrintColor();
+};
+void Vehicle::PrintColor(){
+    cout << "Color: " << Color << endl;
+}
+
+
+
+class Car : virtual public Vehicle {
+
+public:
+    int NofSeats;
+    void SetNumberOfSeats (int NumberOfSeats){
+        this->NofSeats = NumberOfSeats;
+    };
+
+    void PrintNumberOfSeats();
+};
+void Car::PrintNumberOfSeats() {
+    cout << "Number of seats: " << NofSeats << endl;
+}
+
+class Boat : virtual public Vehicle{
+
+public:
+    int doors;
+    void TheDoors(int doors){
+        this->doors = doors;
+    };
+
+    void PrintTheDoors();
 };
 
-class Car : Vehicle {
-private:
+void Boat::PrintTheDoors() {
+    cout << "Number of The Dooors: " << doors << endl;
+}
+
+class Amhibious : public Car,  public Boat {
+
 public:
-    void print(){
-        cout <<""<<endl;
-    }
+    int price;
+    void GetPrice (int price){
+        this->price = price;
+    };
+
+    void PrintPrice();
 };
 
-class Boat : Vehicle{
-private:
-public:
-    void print(){
-        cout <<""<<endl;
-    }
-};
-
-class Amhibious : Car, Boat {
-private:
-public:
-    void print(){
-        cout <<""<<endl;
-    }
-};
-
-
+void Amhibious::PrintPrice() {
+    cout <<"Price: " << price << endl;
+}
 int main() {
 
+    Amhibious *NoWheels = new Amhibious;
+    NoWheels->GetPrice(1000000);
 
+    cout << "Please enter details you want your amhibious to have: " << endl;
+
+    // colorset
+    string col;
+    cout << "We will begin with color: "<< endl;
+    cin>>col;
+
+    NoWheels->SetColor(col);
+
+
+    // seats
+    int seat;
+    cout << "Now enter number of passenggers you want to escape with (dont forget about yourself): "<<endl;
+    cin>>seat;
+
+    NoWheels->SetNumberOfSeats(seat);
+
+    // doors
+    int deDoors;
+    cout << "The Doors now: "<<endl;
+    cin>>deDoors;
+    NoWheels ->TheDoors(deDoors);
+
+    //fin
+    cout << "Okay we done, your vehicle details are printed below: "<< endl;
+
+
+
+
+    NoWheels->PrintPrice();
+    NoWheels->PrintColor();
+    NoWheels->PrintNumberOfSeats();
+    NoWheels->PrintTheDoors();
+
+
+
+
+
+    delete NoWheels;
 
     return 0;
 }
